9ad837ff116d2b940a66660d420388a471bdcfc6
ChinmayJaripatke <31995936+ChinmayJaripatke@users.noreply.github.com>
2021-01-02 11:59:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,89 @@
+// Function for the user to enter the city name:
+
+function showCityName() {
+    cityName = document.getElementById("city").value;
+    console.log(cityName);
+
+    // Selectors:
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationName = document.querySelector(".location-name");
+    let weatherIcon = document.querySelector(".weather-icon");
+    let trip = document.querySelector(".trip");
+
+    const api = `https://api.weatherapi.com/v1/current.json?key= 563ccb8dc2234ebca6e61510210101&q=${cityName}`;
+
+    // Fetching the API:
+
+    fetch(api)
+        .then(reponse => {
+            return reponse.json(); // Converting the response to a json format.
+        })
+        .then(data => {
+            console.log(data);
+
+            // Detstructuring the objects and displaying the data that we need:
+            const { temp_c } = data.current;
+            temperatureDegree.textContent = temp_c;
+
+            const { text } = data.current.condition;
+            temperatureDescription.textContent = text;
+
+            const { name } = data.location;
+            locationName.textContent = name;
+            trip.innerHTML = `<button type="button" class="btn btn-lg btn-success"><a href="https://www.makemytrip.com/hotels/${cityName}-hotels.html">Plan a trip to ${cityName} now!</a></button>`
+
+
+            const { icon } = data.current.condition;
+            weatherIcon.innerHTML = `<img src="${icon}" alt="icon-img">`;
+        })
+
+
+}
+
+// When the page loads, the location of the user will be taken and the weather would be displayed accordingly.
+
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let locationName = document.querySelector(".location-name");
+    let weatherIcon = document.querySelector(".weather-icon");
+    let noLocation = document.querySelector(".no-location");
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
+            const api = `https://api.weatherapi.com/v1/current.json?key= 563ccb8dc2234ebca6e61510210101&q=${lat},${long}`;
+
+            fetch(api)
+                .then(reponse => {
+                    return reponse.json();
+                })
+                .then(data => {
+                    console.log(data);
+
+                    const { temp_c } = data.current;
+                    temperatureDegree.textContent = temp_c;
+
+                    const { text } = data.current.condition;
+                    temperatureDescription.textContent = text;
+
+                    const { name } = data.location;
+                    locationName.textContent = name;
+
+                    const { icon } = data.current.condition;
+                    weatherIcon.innerHTML = `<img src="${icon}" alt="icon-img">`;
+                })
+        })
+
+    }
+
+})
+
+// URL: https://api.weatherapi.com/v1/current.json?key= apiKey&q=London
+// URL MMT : https://www.makemytrip.com/hotels/chennai-hotels.html
